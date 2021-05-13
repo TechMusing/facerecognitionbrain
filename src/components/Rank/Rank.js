a75852a52cb4365a052cb17724a5cd94cb965305
TechMusing <67530350+TechMusing@users.noreply.github.com>
2021-05-13 12:49:01 -0500

    Mock Rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Rank.css";
+
+const Rank = () => {
+  return (
+    <div className="">
+        <div className="white f3">
+            {'Andrea, your current rank is...'}
+        </div>
+        <div className="white f2">
+            {'#5'}
+        </div>
+    </div>
+  );
+};
+
+export default Rank;
