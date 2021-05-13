d24aee61f0b28c957f31fda2912cfe78b5845638
TechMusing <67530350+TechMusing@users.noreply.github.com>
2021-05-13 13:40:14 -0500

    Hook face recognition to image link form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
 
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import React, { Component } from 'react';
+
+const app = new Clarifai.App({
+  apiKey: '7b2f1aa8878e486aaf53a960d0a9fa90'
+ });
 
 const particlesOptions = {
   // background: {
@@ -83,21 +89,54 @@ const particlesOptions = {
     },
   },
   detectRetina: true,
-}
+};
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-          options={particlesOptions}
-      />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log("click");
+    app.models
+    .predict(
+      // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      // to:
+      // .predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+      Clarifai.FACE_DETECT_MODEL,
+      "https://samples.clarifai.com/face-det.jpg")
+    .then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+            options={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
